c7c46794d480fe975cc459f61c17726a0da7aa36
DawiAlotaibi <88381036+DawiAlotaibi@users.noreply.github.com>
2023-09-17 19:59:45 +0300

    chore: Initial commit

--- PrimeDate/Source.cpp
+++ PrimeDate/Source.cpp
@@ -5,98 +5,91 @@
 #include "PrimeUtils.h"
 
 /**
- * Increment the provided date by one day.
- *
- * @param date - Reference to a std::tm object representing the current date.
- * @param end_date - Reference to a std::tm object representing the end date.
- *
- * @return - Returns true if the date is successfully incremented and remains within the given end date.
- *           Returns false once the date surpasses the end date.
+ * Increment the date by one day.
+ * @param date - The current date which will be incremented.
+ * @param end_date - The end date to check if the incremented date hasn't exceeded it.
+ * @return true if the incremented date is before or on the end_date, otherwise false.
  */
 bool incrementDate(std::tm& date, const std::tm& end_date) {
-    // An array storing the number of days in each month.
+    // Days in each month.
     static const int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-    date.tm_mday++;  // Increment the day
+    // Increment the day.
+    date.tm_mday++;
 
-    // Check if the day exceeds the maximum day of the month, considering leap years for February.
+    // If day exceeds the month's days, reset day to 1 and move to next month.
+    // Also handle leap year for February.
     if (date.tm_mday > daysInMonth[date.tm_mon] + ((date.tm_mon == 1 && date.tm_year % 4 == 0 && (date.tm_year % 100 != 0 || (date.tm_year + 1900) % 400 == 0)) ? 1 : 0)) {
-        date.tm_mday = 1;  // Reset day to 1
-        date.tm_mon++;     // Move to the next month
+        date.tm_mday = 1;
+        date.tm_mon++;
 
-        // Check if the month goes beyond December.
+        // If month exceeds 11 (December), reset month to 0 (January) and move to next year.
         if (date.tm_mon > 11) {
-            date.tm_mon = 0;       // Reset month to January
-            date.tm_year++;        // Move to the next year
+            date.tm_mon = 0;
+            date.tm_year++;
         }
     }
 
-    // Check if the date surpasses the provided end date.
-    if (date.tm_year > end_date.tm_year) return false;
-    if (date.tm_year == end_date.tm_year && date.tm_mon > end_date.tm_mon) return false;
-    if (date.tm_year == end_date.tm_year && date.tm_mon == end_date.tm_mon && date.tm_mday > end_date.tm_mday) return false;
-
-    return true;
+    // Check if the current date exceeds the end date.
+    return !(date.tm_year > end_date.tm_year ||
+        (date.tm_year == end_date.tm_year && date.tm_mon > end_date.tm_mon) ||
+        (date.tm_year == end_date.tm_year && date.tm_mon == end_date.tm_mon && date.tm_mday > end_date.tm_mday));
 }
 
 /**
- * Parse a date string in the format "YYYY-MM-DD" to a std::tm object.
- *
- * @param date_str - Date string in the format "YYYY-MM-DD".
- *
- * @return - Returns a std::tm object representing the parsed date.
+ * Parse the date from a string.
+ * @param input - The input string in format YYYY-MM-DD.
+ * @param date - The parsed date will be stored in this variable.
+ * @return true if parsing was successful, otherwise false.
  */
-std::tm parseDate(const std::string& date_str) {
-    std::tm date = {};
-    std::stringstream ss(date_str);
-
-    int year, month, day;
-    char dash;
-
-    ss >> year >> dash >> month >> dash >> day;
-
-    date.tm_year = year - 1900;  // tm_year is the year since 1900
-    date.tm_mon = month - 1;     // tm_mon uses a range of [0, 11]
-    date.tm_mday = day;
-
-    return date;
+bool parseDate(const std::string& input, std::tm& date) {
+    std::istringstream ss(input);
+    char delimiter;
+    return (ss >> date.tm_year >> delimiter >> date.tm_mon >> delimiter >> date.tm_mday) && delimiter == '-' && !ss.fail();
 }
 
 int main() {
-    // Prompt user for the start date.
-    std::cout << "Enter the start date in the format YYYY-MM-DD: ";
-    std::string start_date_str;
-    std::cin >> start_date_str;
-
-    // Prompt user for the end date.
-    std::cout << "Enter the end date in the format YYYY-MM-DD: ";
-    std::string end_date_str;
-    std::cin >> end_date_str;
-
-    // Parse the provided start and end dates.
-    std::tm start_date = parseDate(start_date_str);
-    std::tm end_date = parseDate(end_date_str);
-
-    // Loop from the start date to the end date.
     while (true) {
-        // If the day is even, skip to the next iteration.
-        if (start_date.tm_mday % 2 != 0) { // This line checks if the day is odd
-            // Construct the number in the format YYYYMMDD.
-            int number = (start_date.tm_year + 1900) * 10000 +
-                (start_date.tm_mon + 1) * 100 +
-                start_date.tm_mday;
+        std::string startStr, endStr;
 
-            // Check if the constructed number is prime.
-            if (isPrime(number)) {
-                std::cout << start_date.tm_mday << "/" << start_date.tm_mon + 1 << "/" << start_date.tm_year + 1900 << " (" << number << ") is a prime date." << std::endl;
-            }
+        // Prompt user to enter a start date or exit the program.
+        std::cout << "Enter the start date in the format YYYY-MM-DD or type 'quit' or 'q' or an empty string to exit: ";
+        std::getline(std::cin, startStr);
+        if (startStr == "quit" || startStr == "q" || startStr == "") {
+            break;
         }
 
-        if (!incrementDate(start_date, end_date)) {
-            break; // Exit the loop if we've exceeded the end date
+        // Prompt user to enter an end date.
+        std::cout << "Enter the end date in the format YYYY-MM-DD or type 'quit' or 'q' or an empty string to exit: ";
+        std::getline(std::cin, endStr);
+        if (endStr == "quit" || endStr == "q" || endStr == "") {
+            break;
+        }
+        // Parse the start and end dates.
+        std::tm startDate = {}, endDate = {};
+        if (!parseDate(startStr, startDate) || !parseDate(endStr, endDate)) {
+            std::cout << "Invalid date format. Please try again." << std::endl;
+            continue;
+        }
+
+        // Adjust year values (tm_year is years since 1900).
+        startDate.tm_year -= 1900;
+        endDate.tm_year -= 1900;
+
+        // Adjust month values (tm_mon is 0-11).
+        startDate.tm_mon--;
+        endDate.tm_mon--;
+
+        // Loop through each date between start and end dates and check for prime dates.
+        while (incrementDate(startDate, endDate)) {
+            // Calculate the numeric representation of the date.
+            int number = (startDate.tm_year + 1900) * 10000 + (startDate.tm_mon + 1) * 100 + startDate.tm_mday;
+
+            // Check if the date is prime and display it.
+            if (isPrime(number))
+                std::cout << startDate.tm_mday << "/" << startDate.tm_mon + 1 << "/" << startDate.tm_year + 1900 << " (" << number << ") is a prime date." << std::endl;
         }
     }
 
-
-    return 0;  // Indicate successful completion.
+    return 0;
 }
